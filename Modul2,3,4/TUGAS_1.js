b10d86300690467f5ab8b5af10e74eda85836da5
AlexRoscov <mraihanws@gmail.com>
2026-03-16 17:40:37 +0700

    Tugas 1

---
+++ Modul2,3,4/TUGAS_1.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("sidebar").style.transform = "translateX(0)";
+}
+
+function closeNav() {
+    document.getElementById("sidebar").style.transform = "translateX(-100%)";
+}
